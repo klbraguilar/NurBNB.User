c30e4825a0818d95b362bad1dfac1d58c414e913
klbraguilar <klbraguilar@gmail.com>
2024-01-27 06:43:38 -0800

    adding tests

---
+++ NurBNB.Usuario.Test/Application/UseCases/CheckInOut/Command/CrearCheckInCommand_test.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using NurBNB.Usuario.Appplication.UseCases.CheckInOut.Command.CrearCheckInCommand;
+using NurBNB.Usuario.Domain.Factories;
+using NurBNB.Usuario.Domain.Model.CheckInOut;
+using NurBNB.Usuario.Domain.Model.Users;
+using NurBNB.Usuario.Domain.Repositories;
+using Restaurant.SharedKernel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NurBNB.Usuario.Test.Application.UseCases.CheckInOut.Command
+{
+    public class CrearCheckInCommand_test
+    {
+        [Fact]
+        public async Task Handle_ValidRequest_CreatesCheckInAndReturnsId()
+        {
+            // Arrange
+            var guestId = Guid.NewGuid();
+            var reservaId = Guid.NewGuid();
+            var contacto = "John Doe";
+            var fechaLlegada = DateTime.Now;
+
+            var crearCheckInCommand = new CrearCheckInCommand
+            {
+                guestId = guestId,
+                reservaId = reservaId,
+                contacto = contacto,
+                fechaLlegada = fechaLlegada
+            };
+
+            var mockCheckInRepository = new Mock<ICheckInRepository>();
+            var mockCheckInFactory = new Mock<ICheckInFactory>();
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+            var checkInId = Guid.NewGuid();
+            var checkInCreado = new CheckIn(crearCheckInCommand.guestId, crearCheckInCommand.reservaId, crearCheckInCommand.contacto, crearCheckInCommand.fechaLlegada);
+
+
+            mockCheckInFactory.Setup(factory => factory.Crear(guestId, reservaId, contacto, fechaLlegada))
+                .Returns(checkInCreado);
+
+            var crearCheckInHandler = new CrearCheckInHandler(
+                mockCheckInRepository.Object,
+                mockCheckInFactory.Object,
+                mockUnitOfWork.Object
+            );
+
+            // Act
+            var result = await crearCheckInHandler.Handle(crearCheckInCommand, CancellationToken.None);
+
+            // Assert
+            Assert.Equal(checkInCreado.Id, result);
+            mockCheckInFactory.Verify(factory => factory.Crear(guestId, reservaId, contacto, fechaLlegada), Times.Once);
+            mockCheckInRepository.Verify(repository => repository.CreateAsync(checkInCreado), Times.Once);
+            mockUnitOfWork.Verify(unitOfWork => unitOfWork.Commit(), Times.Once);
+        }
+    }
+}
---
+++ NurBNB.Usuario.Test/Domain/Factory/CheckInFactory_test.cs
@@ -0,0 +1,55 @@
+﻿using NurBNB.Usuario.Domain.Factories;
+using NurBNB.Usuario.Domain.Model.CheckInOut;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NurBNB.Usuario.Test.Domain.Factory
+{
+    public class CheckInFactory_test
+    {
+        [Fact]
+        public void CheckInFactory_Crear_ReturnsCheckInWithCorrectProperties()
+        {
+            // Arrange
+            var factory = new CheckInFactory();
+            var guestId = Guid.NewGuid();
+            var reservaId = Guid.NewGuid();
+            var contacto = "John Doe";
+            var fechaLlegada = DateTime.Now;
+
+            // Act
+            var checkIn = factory.Crear(guestId, reservaId, contacto, fechaLlegada);
+
+            // Assert
+            Assert.NotNull(checkIn);
+            Assert.IsType<CheckIn>(checkIn);
+            Assert.Equal(guestId, checkIn.GuestId);
+            Assert.Equal(reservaId, checkIn.ReservaId);
+            Assert.Equal(contacto, checkIn.Contacto);
+            Assert.Equal(fechaLlegada, checkIn.FechaLlegada);
+        }
+
+        // Puedes agregar más pruebas según los requisitos específicos de tu aplicación
+
+        [Fact]
+        public void CheckInFactory_Crear_ReturnsUniqueCheckInInstances()
+        {
+            // Arrange
+            var factory = new CheckInFactory();
+            var guestId = Guid.NewGuid();
+            var reservaId = Guid.NewGuid();
+            var contacto = "Jane Doe";
+            var fechaLlegada = DateTime.Now;
+
+            // Act
+            var checkIn1 = factory.Crear(guestId, reservaId, contacto, fechaLlegada);
+            var checkIn2 = factory.Crear(guestId, reservaId, contacto, fechaLlegada);
+
+            // Assert
+            Assert.NotEqual(checkIn1, checkIn2);
+        }
+    }
+}
